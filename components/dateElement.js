9056e7c3af35103065cc67da4330bb641fbdfdb2
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-07-27 22:28:18 -0400

    Proyecto terminado

---
+++ components/dateElement.js
@@ -0,0 +1,6 @@
+export default (date) => {
+    const date_element = document.createElement("li");
+    date_element.classList.add("date");
+    date_element.innerHTML = date;
+    return date_element;
+};
